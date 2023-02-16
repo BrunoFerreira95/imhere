48c1440881f86223a61b1af7ba9d62b05b9d73dc
Bruno Ferreira <brunoferreirapedraca@gmail.com>
2023-02-16 13:28:17 -0300

    Finalizando App - Salvando participante no estado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,14 +5,16 @@ import { styles } from './styles'
 import { Participant } from '../../components/Participant'
 
 export function Home() {
-  const [participants, setParticipants] = useState(['Bruno'])
+  const [participants, setParticipants] = useState<string[]>([])
+  const [participantName, setParticipantName] = useState('')
 
   function handleParticipantAdd(){
-    if(participants.includes("Rodrigo")) {
+    if(participants.includes(participantName)) {
       return Alert.alert('Participante existe', 'Já existe um participante na lista com esse nome.')
     }
 
-    setParticipants(prevState => [...prevState, 'Ana'])
+    setParticipants(prevState => [...prevState, participantName])
+    setParticipantName('')
   }
 
   function handlePrticipantRemove(name: string){
@@ -41,6 +43,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B" 
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
